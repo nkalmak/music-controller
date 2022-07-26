de741c6f789aff32b6359f1fb7f1f49e1b4095cb
Nicholas <nkalmak@gmail.com>
2022-07-26 11:53:57 -0700

    updated urls

--- music_controller/api/urls.py
+++ music_controller/api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import RoomView
 
 urlpatterns = [
-    path('room', RoomView.as_view())
+    path('room', RoomView.as_view()),
+    path('create-room', RoomView.as_view())
 ]
